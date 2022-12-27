a51fd790362b70b87967356e1f2ec15f3d792eb9
Anel Husakovic <anel@eacon.ba>
2022-12-27 15:37:46 +0100

    Register user with unhashed password
    Steps:
    1. Create serializer
    2. Use serializer
    3. Test with postman
    - create new workspace -> create post method on:
    localhost:8000/api/register route with Body of type JSON and insert data
    in form
    ```bash
    {
        "name":"Anel Husakovic",
        "email":"anel@eacon.ba",
        "password":"mycoolpasssword"
    }
    ```
    Output returned on django server (before was text type instead of json)
    ```bash
    [27/Dec/2022 14:30:26] "POST /api/register HTTP/1.1" 415 62
    Unsupported Media Type: /api/register
    [27/Dec/2022 14:33:48] "POST /api/register HTTP/1.1" 415 62
    [27/Dec/2022 14:33:56] "POST /api/register HTTP/1.1" 200 85
    ```
    
    Next hash the password

--- myEnrollment/myEnrollment/urls.py
+++ myEnrollment/myEnrollment/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Include `urls` file from application
+    # to accept all end-points of user
+    path('api/', include('teachersAuth.urls')),
 ]
