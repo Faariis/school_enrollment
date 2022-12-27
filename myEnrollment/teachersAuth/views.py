9dc2ba6c43fdaa8632d5feabe772364ae93b3a19
Anel Husakovic <anel@eacon.ba>
2022-12-27 15:37:46 +0100

    Register user with unhashed password
    Steps:
    1. Create serializer
    2. Use serializer
    3. Test with postman
    - create new workspace -> create post method on:
    localhost:8000/api/register route with Body of type JSON and insert data
    in form
    ```bash
    {
        "name":"Anel Husakovic",
        "email":"anel@eacon.ba",
        "password":"mycoolpasssword"
    }
    ```
    Output returned on django server (before was text type instead of json)
    ```bash
    [27/Dec/2022 14:30:26] "POST /api/register HTTP/1.1" 415 62
    Unsupported Media Type: /api/register
    [27/Dec/2022 14:33:48] "POST /api/register HTTP/1.1" 415 62
    [27/Dec/2022 14:33:56] "POST /api/register HTTP/1.1" 200 85
    ```
    
    Next hash the password

--- myEnrollment/teachersAuth/views.py
+++ myEnrollment/teachersAuth/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from .serializers import TeacherSerializer
 
 # Create your views here.
+class RegisterView(APIView):
+    # get and post function in APIView
+    def post(self, request):
+        # To create the user first create serializer.py 
+        serializer= TeacherSerializer(data= request.data)
+        serializer.is_valid( raise_exception= True)
+        serializer.save()
+        # return it, to test it install postman
+        return Response(serializer.data)
+        # pass
+
+
