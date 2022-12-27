ea028f6b89f89aa84c948ba2081dee431ce39563
Anel Husakovic <anel@eacon.ba>
2022-12-27 15:06:49 +0100

    Connect to MariaDB and migrate
    Install mariadb server
    1. Set to 10.6 go to mariadb.org and create mariadb.list / update and install the server
    ```bash
    $ sudo curl -o /etc/apt/trusted.gpg.d/mariadb_release_signing_key.asc 'https://mariadb.org/mariadb_release_signing_key.asc'
    $ sudo sh -c "echo 'deb https://ftp.bme.hu/pub/mirrors/mariadb/repo/10.6/ubuntu focal main' >>/etc/apt/sources.list.d/mariadb.org.list"
    $ cat /etc/apt/sources.list.d/mariadb.org.list
    deb [arch=amd64] https://ftp.bme.hu/pub/mirrors/mariadb/repo/10.6/ubuntu focal main
    ```
    2. create the user and DB
    ```bash
    $ sudo mariadb -uroot
    ```
    ```sql
    create user anel@localhost identified by 'pass';
    create database myEnrollmentDB;
    grant all privileges on myEnrollmentDB.* to anel@localhost;
    ```
    3. Work with mariadb
    - We need mariadb_config, found in `libmariadb-dev` (MariaDB connector C)
    ```bash
    $ sudo apt install libmariadb-dev
    $ dpkg -L libmariadb-dev|grep config
    /usr/bin/mariadb_config
    /usr/include/mariadb/my_config.h
    /usr/lib/x86_64-linux-gnu/pkgconfig
    /usr/lib/x86_64-linux-gnu/pkgconfig/libmariadb.pc
    /usr/share/man/man1/mysql_config.1.gz
    /usr/bin/mariadb-config
    /usr/share/man/man1/mariadb-config.1.gz
    /usr/share/man/man1/mariadb_config.1.gz
    ```
    - Then, `mysqlclient and mariadb`
    ```
    python -m pip install mysqlclient mariadb
    ```
    
    4. Create migration
    ```bash
    $ ./manage.py makemigrations --dry-run
    Migrations for 'teachersAuth':
      teachersAuth/migrations/0001_initial.py
        - Create model Teacher
    
    ```
    
    5. Run migration
    ```bash
    
    $ ./manage.py migrate --plan
    Planned operations:
    contenttypes.0001_initial
        Create model ContentType
        Alter unique_together for contenttype (1 constraint(s))
    contenttypes.0002_remove_content_type_name
        Change Meta options on contenttype
        Alter field name on contenttype
        Raw Python operation
        Remove field name from contenttype
    
    ```
    
    Check in DB
    ```
    MariaDB [myEnrollmentDB]> show tables;
    +---------------------------------------+
    | Tables_in_myEnrollmentDB              |
    +---------------------------------------+
    | auth_group                            |
    | auth_group_permissions                |
    | auth_permission                       |
    | django_admin_log                      |
    | django_content_type                   |
    | django_migrations                     |
    | django_session                        |
    | teachersAuth_teacher                  |
    | teachersAuth_teacher_groups           |
    | teachersAuth_teacher_user_permissions |
    +---------------------------------------+
    10 rows in set (0.000 sec)
    
    ```

--- myEnrollment/teachersAuth/models.py
+++ myEnrollment/teachersAuth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# Followed the tutorial https://www.youtube.com/watch?v=PUzgZrS_piQ&list=PLlameCF3cMEu-LbsQYUDUVkiZ2jc2rpLx
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Teacher(AbstractUser):
+    name= models.CharField(max_length = 50)
+    email= models.CharField(max_length = 50, unique= True)
+    password= models.CharField(max_length = 50)
+    # Django by default creates username we have to overrdie it
+    # We want that Django logs in with email and password
+    username= None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
