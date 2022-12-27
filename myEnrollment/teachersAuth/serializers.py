abef81080e175d8e70595a93556aec7fb9ba9b72
Anel Husakovic <anel@eacon.ba>
2022-12-27 16:47:10 +0100

    Hashed password works!
    - Function `create()` is method for the class not independent method

--- myEnrollment/teachersAuth/serializers.py
+++ myEnrollment/teachersAuth/serializers.py
@@ -11,18 +11,17 @@ class TeacherSerializer(serializers.ModelSerializer):
             'password':{'write_only':True}
         }
 
-# Override default create method that is between view and model creation
-def create(self, validated_data):
-    # extract pass
-    print("**Validated data before: ", **validated_data)
-    teacher= self.Meta.model(**validated_data)
-    my_password= validated_data.pop('password', None)
-    print("Validated data after: ", validated_data)
-    print("**Validated data after: ", **validated_data)
-    # create teacher and pass data without password
-    if my_password:
-        # set_password() provided by Django and hashed
-        teacher.set_password(my_password)
-        print("U funckiji je moj pass: ", my_password)
-    teacher.save()
-    return teacher
+    # Override default create method that is between view and model creation
+    def create(self, validated_data):
+        # extract pass
+        my_password= validated_data.pop('password', None)
+        print("Validated data after: ", validated_data)
+        # create teacher and pass data without password
+        instance= self.Meta.model(**validated_data)
+    
+        if my_password:
+            # set_password() provided by Django and hashed
+            instance.set_password(my_password)
+            print("U funckiji je moj pass: ", my_password)
+        instance.save()
+        return instance
