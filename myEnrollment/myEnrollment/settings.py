6d556150cf9c80d62aec28e1a6389ac382bfcca8
Anel Husakovic <anel@eacon.ba>
2023-01-06 01:59:49 +0100

    User JWT for header type not Bearer, but make request with Bearer not [JWT,TOKEN]
    - Example of request
    ```bash
     curl -X GET http://51.15.114.199:3534/api/teachers/ -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcyOTczNTc5LCJpYXQiOjE2NzI5NjYzNzksImp0aSI6IjE5OWZhNzQzODkyYTQxMmNhNzEzMDIwMjRiNzkxM2I1IiwidXNlcl9pZCI6Mn0.KZ-9bvwXyrDKWUFblIB6E4ffKk9-honNslDmXNHALNo"
    [{"id":3,"is_superuser":false,"first_name":"","last_name":""},{"id":1,"is_superuser":true,"first_name":"Ismet","last_name":"Glumcevic"},{"id":2,"is_superuser":true,"first_name":"Ismet","last_name":"Glumcevic"}]
    ```

--- myEnrollment/myEnrollment/settings.py
+++ myEnrollment/myEnrollment/settings.py
@@ -185,7 +185,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "SIGNING_KEY": SECRET_KEY,
-    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_TYPES": ("JWT",),
     # "Bearer <Token>"
 }
 # JWTStatelessUserAuthentication
