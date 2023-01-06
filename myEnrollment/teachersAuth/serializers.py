0e689e3a0db655b04c4cbe89cce653ef03d54dc4
Anel Husakovic <anel@eacon.ba>
2023-01-06 01:59:49 +0100

    Using fixtures and hashed password of fixture for proper admin and rest

--- myEnrollment/teachersAuth/serializers.py
+++ myEnrollment/teachersAuth/serializers.py
@@ -10,4 +10,7 @@ class TeacherLoginSerializer(serializers.ModelSerializer):
     canton_code = serializers.CharField(source='Canton._canton_code')
     class Meta:
         model= Teacher() # get_user_model()
-        fields=('id', 'is_superuser', 'first_name', 'last_name', 'canton_code')
+        fields=('id', 'is_superuser', 'first_name', 'last_name')
+    def create(self, validated_data):
+        user = Teacher.objects.create_user(validated_data['email'], validated_data["password"])
+        return user
