e5ec3f950bf3e712794dff62d827e49ca125e631
Anel Husakovic <anel@eacon.ba>
2023-01-10 07:39:47 +0100

    Add reverse relation to serializer.Models

--- myEnrollment/secondarySchools/serializers.py
+++ myEnrollment/secondarySchools/serializers.py
@@ -8,7 +8,9 @@ from secondarySchools.models import (
 class CantonSerializer(serializers.ModelSerializer):
     class Meta:
         model= Canton
-        fields= "__all__"
+        # To include reverse relation of child tables explicitly add in list
+        fields= ['_canton_code', 'canton_name', 'country', 'school_canton']
+        # fields="__all__"
 
 class CoursesSecondarySchoolSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,11 +18,12 @@ class CoursesSecondarySchoolSerializer(serializers.ModelSerializer):
         # fields= "__all__"
         exclude=('school_id',)
 
+
 # Nested relations of serialiser https://www.django-rest-framework.org/api-guide/relations/#nested-relationships
 class SecondarySchoolSerializer(serializers.ModelSerializer):
     # To get many teacher from this school. Variable name MUST be same as related_name
     # read_only means that we cannot add cantons through post request
-    school_canton= CantonSerializer(read_only= True) # this will return all data about teacher
+    # school_canton= CantonSerializer(read_only= True) # this will return all data about teacher
     # To control what to show: __str__
     # school_id= serializers.StringRelatedField(many=True) # this will return teacher.__str__
     # school_id= serializers.PrimaryKeyRelatedField(many=True, ready_only=True) # returns only primary keys
@@ -30,6 +33,8 @@ class SecondarySchoolSerializer(serializers.ModelSerializer):
     # school_id= serializers.HyperlinkedIdentityField(view_name='track-list') # used as link identity to id
     class Meta:
         model= SecondarySchool
-        fields= "__all__"
-        depth= 2
+        # fields= "__all__"
+        # depth= 1
+        fields= ['id', 'school_name', 'school_address', 'school_canton_code',
+                 'courses_secondary']
 
