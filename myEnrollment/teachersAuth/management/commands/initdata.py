3a48dbcc0730f327604673bf87cfed54e7c5177b
Anel Husakovic <anel@eacon.ba>
2023-01-12 08:54:21 +0100

    Create fixutes per application and load them in initdata command

--- myEnrollment/teachersAuth/management/commands/initdata.py
+++ myEnrollment/teachersAuth/management/commands/initdata.py
@@ -1,13 +1,29 @@
 from django.core.management import BaseCommand, call_command
 from teachersAuth.models import Teacher # from yourapp.models import User # if you have a custom user
-
+from secondarySchools.fixtures import *
 
 class Command(BaseCommand):
     help = "DEV COMMAND: Fill databasse with a set of data for testing purposes"
 
     def handle(self, *args, **options):
-        call_command('loaddata','initdata')
+        # app: secondarySchools fixtures
+        call_command('loaddata','initdata-cantons')
+        call_command('loaddata','initdata-secondary-schools')
+        call_command('loaddata','initdata-courses')
+        # app: teachersAuth fixtures
+        call_command('loaddata','initdata-teacher')
         # Fix the passwords of fixtures
         for t in Teacher.objects.all():
             t.set_password(t.password)
             t.save()
+
+
+        # #initdata file is created combining multiple files from other fixtures
+        # app_name = __package__.split('.')[0]
+        # myFile= app_name+"/fixtures/initdata.json"
+        # # open all files
+        # with open('first.txt','r') as firstfile, open(myFile,'a') as destFile:
+        #     # read content from first file
+        #     for line in firstfile:
+        #         # append content to second file
+        #         destFile.write(line)
